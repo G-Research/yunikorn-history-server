bbd4467a2bb16fbeca1e745df57d9a963966e36e
Sudipto Baral <sudiptobaral.me@gmail.com>
2024-06-12 10:27:05 -0400

    Implement env parsing from env vars when config file is not passed (#29)
    * Implement env parsing from env vars when config file is not passed

--- cmd/event-collector/main.go
+++ cmd/event-collector/main.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 
 	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/env"
 	"github.com/knadh/koanf/providers/file"
-	"github.com/knadh/koanf/v2"
+	koanf "github.com/knadh/koanf/v2"
 
 	"github.com/G-Research/yunikorn-history-server/internal/config"
 	"github.com/G-Research/yunikorn-history-server/internal/repository"
@@ -26,21 +28,40 @@ var (
 	eventCounts   config.EventTypeCounts
 )
 
-func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "usage: %s path/to/config.yml\n", os.Args[0])
-		os.Exit(1)
-	}
-
-	cfgFile := os.Args[1]
-	if _, err := os.Stat(cfgFile); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: cannot open config file %s: %v\n", cfgFile, err)
-		os.Exit(1)
-	}
-
+func loadConfig(cfgFile string) (*koanf.Koanf, error) {
 	k := koanf.New(".")
-	if err := k.Load(file.Provider(cfgFile), yaml.Parser()); err != nil {
-		fmt.Fprintf(os.Stderr, "error loading file: %v", err)
+
+	// Try to load from the config file if it's provided
+	if cfgFile != "" {
+		if _, err := os.Stat(cfgFile); err == nil {
+			if err := k.Load(file.Provider(cfgFile), yaml.Parser()); err == nil {
+				// Successfully loaded from file, return
+				return k, nil
+			}
+		}
+	}
+
+	// If there's no config file or there's an error reading it, default to env vars
+	// YHS_PARENT1_CHILD1_NAME will be converted into "parent1.child1.name"
+	if err := k.Load(env.Provider("YHS_", ".", func(s string) string {
+		return strings.Replace(strings.ToLower(
+			strings.TrimPrefix(s, "YHS_")), "_", ".", -1)
+	}), nil); err != nil {
+		return nil, fmt.Errorf("error loading environment variables: %v", err)
+	}
+
+	return k, nil
+}
+
+func main() {
+	cfgFile := ""
+	if len(os.Args) == 2 {
+		cfgFile = os.Args[1]
+	}
+
+	k, err := loadConfig(cfgFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
 
