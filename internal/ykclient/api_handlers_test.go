e77bb03d425c3ff31712ed333f0523dabb69064e
sudipto baral <sudiptobaral.me@gmail.com>
2024-06-23 11:41:49 -0400

    Add unit test for GetApplications endpoint #15

--- internal/ykclient/api_handlers_test.go
+++ internal/ykclient/api_handlers_test.go
@@ -120,3 +120,113 @@ func Test_GetApplication(t *testing.T) {
 		})
 	}
 }
+
+func Test_GetApplications(t *testing.T) {
+	tests := []struct {
+		name      string
+		partName  string
+		queueName string
+		expected  []*dao.ApplicationDAOInfo
+	}{
+		{
+			name:      "All Fields Specified",
+			partName:  "specialPartition",
+			queueName: "myQueue",
+			expected: []*dao.ApplicationDAOInfo{
+				{
+					ApplicationID: "app1",
+					Partition:     "specialPartition",
+					QueueName:     "myQueue",
+				},
+			},
+		},
+		{
+			name:      "Empty Partition Name",
+			partName:  "",
+			queueName: "myQueue",
+			expected: []*dao.ApplicationDAOInfo{
+				{
+					ApplicationID: "app1",
+					Partition:     "default",
+					QueueName:     "myQueue",
+				},
+			},
+		},
+		{
+			name:      "Empty Queue Name",
+			partName:  "myPartition",
+			queueName: "",
+			expected: []*dao.ApplicationDAOInfo{
+				{
+					ApplicationID: "app1",
+					Partition:     "myPartition",
+					QueueName:     "myPartition.default",
+				},
+			},
+		},
+	}
+
+	partQueueAppRe := regexp.MustCompile(`/ws/v1/partition/(\w+)/queue/(\w+)/applications`)
+	partAppRe := regexp.MustCompile(`/ws/v1/partition/(\w+)/applications/active`)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				var responseApp []*dao.ApplicationDAOInfo
+
+				matches := partQueueAppRe.FindStringSubmatch(r.URL.Path)
+				if matches != nil {
+					responseApp = []*dao.ApplicationDAOInfo{
+						{
+							Partition:     matches[1],
+							QueueName:     matches[2],
+							ApplicationID: "app1",
+						},
+					}
+				} else {
+					matches = partAppRe.FindStringSubmatch(r.URL.Path)
+					if matches == nil {
+						http.Error(w, errors.New("invalid request path").Error(), http.StatusNotFound)
+						return
+					}
+
+					responseApp = []*dao.ApplicationDAOInfo{
+						{
+							Partition:     matches[1],
+							QueueName:     fmt.Sprintf("%s.%s", matches[1], "default"),
+							ApplicationID: "app1",
+						},
+					}
+
+				}
+				err := json.NewEncoder(w).Encode(&responseApp)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+				}
+
+			}))
+
+			defer ts.Close()
+			serverURL, err := url.Parse(ts.URL)
+			require.NoError(t, err)
+
+			portNum, err := strconv.Atoi(serverURL.Port())
+			require.NoError(t, err)
+
+			ykclient := Client{
+				httpProto: serverURL.Scheme,
+				ykHost:    serverURL.Hostname(),
+				ykPort:    portNum,
+				repo:      nil,
+				appMap:    map[string]*dao.ApplicationDAOInfo{},
+			}
+
+			apps, err := ykclient.GetApplications(context.Background(), tt.partName, tt.queueName)
+			require.NoError(t, err)
+			assert.NotNil(t, apps)
+			for i := range tt.expected {
+				assert.Equal(t, *tt.expected[i], *apps[i])
+			}
+		})
+	}
+}
