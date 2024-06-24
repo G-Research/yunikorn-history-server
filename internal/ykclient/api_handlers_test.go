75d3ecf9a54b3cb933433af86bbb8db7a6aa1a51
sudipto baral <sudiptobaral.me@gmail.com>
2024-06-23 22:22:09 -0400

    Add unit test for GetPartitionNodes #15

--- internal/ykclient/api_handlers_test.go
+++ internal/ykclient/api_handlers_test.go
@@ -416,3 +416,96 @@ func Test_GetPartitionQueues(t *testing.T) {
 		})
 	}
 }
+
+func Test_GetPartitionNodes(t *testing.T) {
+	tests := []struct {
+		name     string
+		setup    func() *httptest.Server
+		expected []*dao.NodeDAOInfo
+		wantErr  bool
+	}{
+		{
+			name: "200 OK Response",
+			setup: func() *httptest.Server {
+				return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					response := []*dao.NodeDAOInfo{
+						{
+							NodeID: "node1",
+						},
+						{
+							NodeID: "node2",
+						},
+					}
+					err := json.NewEncoder(w).Encode(response)
+					if err != nil {
+						http.Error(w, err.Error(), http.StatusInternalServerError)
+					}
+				}))
+			},
+			expected: []*dao.NodeDAOInfo{
+				{
+					NodeID: "node1",
+				},
+				{
+					NodeID: "node2",
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Server Error",
+			setup: func() *httptest.Server {
+				return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					http.Error(w, "server error", http.StatusInternalServerError)
+				}))
+			},
+			expected: nil,
+			wantErr:  true,
+		},
+		{
+			name: "Unexpected JSON",
+			setup: func() *httptest.Server {
+				return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					type unexpected struct {
+						Unexpected string `json:"unexpected"`
+					}
+					err := json.NewEncoder(w).Encode(&unexpected{Unexpected: "unexpected"})
+					if err != nil {
+						http.Error(w, err.Error(), http.StatusInternalServerError)
+					}
+				}))
+			},
+			expected: nil,
+			wantErr:  true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ts := tt.setup()
+			defer ts.Close()
+
+			serverURL, err := url.Parse(ts.URL)
+			require.NoError(t, err)
+
+			portNum, err := strconv.Atoi(serverURL.Port())
+			require.NoError(t, err)
+
+			ykclient := Client{
+				httpProto: serverURL.Scheme,
+				ykHost:    serverURL.Hostname(),
+				ykPort:    portNum,
+				repo:      nil,
+				appMap:    map[string]*dao.ApplicationDAOInfo{},
+			}
+
+			nodes, err := ykclient.GetPartitionNodes(context.Background(), "testPartition")
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				assert.Equal(t, tt.expected, nodes)
+			}
+		})
+	}
+}
