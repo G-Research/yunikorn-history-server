7506405dbd9c164d5d9c9610bd75dc958a4d0dba
Dejan Zele Pejchev <pejcev.dejan@gmail.com>
2024-07-08 01:01:22 +0200

    separate run command into a func

--- cmd/yunikorn-history-server/commands/root.go
+++ cmd/yunikorn-history-server/commands/root.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"context"
 	"fmt"
 	"github.com/G-Research/yunikorn-history-server/internal/database/postgres"
 	"github.com/G-Research/yunikorn-history-server/internal/database/repository"
@@ -35,66 +36,71 @@ var rootCmd = &cobra.Command{
 			return err
 		}
 
-		log.Init(&cfg.LogConfig)
-
-		log.Logger.Infow(
-			"starting yunikorn history server",
-			"version", info.Version, "buildTime", info.BuildTime, "commit", info.Commit,
-		)
-
-		ctx, cancel := signal.NotifyContext(cmd.Context(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
-		defer cancel()
-
-		log.ToContext(ctx, log.Logger)
-
-		pool, err := postgres.NewConnectionPool(ctx, &cfg.PostgresConfig)
-		if err != nil {
-			return fmt.Errorf("cannot parse Postgres connection config: %w", err)
-		}
-		mainRepository, err := repository.NewPostgresRepository(pool)
-		if err != nil {
-			log.Logger.Error("could not create db repository")
-			panic(err)
-		}
-		eventRepository := repository.NewInMemoryEventRepository()
-
-		g := run.Group{}
-
-		client := yunikorn.NewRESTClient(&cfg.YunikornConfig)
-		service := yunikorn.NewService(mainRepository, eventRepository, client)
-		g.Add(
-			func() error {
-				return service.RunEventCollector(ctx)
-			},
-			func(err error) {
-				service.Shutdown()
-			},
-		)
-		g.Add(
-			func() error {
-				return service.RunDataSync(ctx)
-			},
-			func(err error) {},
-		)
-
-		healthService := health.New(info.Version, health.NewYunikornComponent(client), health.NewPostgresComponent(pool))
-
-		ws := webservice.NewWebService(cfg.Port, mainRepository, eventRepository, healthService)
-		g.Add(
-			func() error {
-				return ws.Start(ctx)
-			},
-			func(err error) {
-				_ = ws.Shutdown(ctx)
-			},
-		)
-
-		if err = g.Run(); err != nil {
-			log.Logger.Warnf("group stopped because of an error: %v", err)
-		}
-
-		return nil
+		return Run(context.Background(), cfg)
 	},
+}
+
+// Run is the main entry point for the yunikorn history server.
+func Run(ctx context.Context, cfg *config.Config) error {
+	log.Init(&cfg.LogConfig)
+
+	log.Logger.Infow(
+		"starting yunikorn history server",
+		"version", info.Version, "buildTime", info.BuildTime, "commit", info.Commit,
+	)
+
+	ctx, cancel := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+	defer cancel()
+
+	log.ToContext(ctx, log.Logger)
+
+	pool, err := postgres.NewConnectionPool(ctx, &cfg.PostgresConfig)
+	if err != nil {
+		return fmt.Errorf("cannot parse Postgres connection config: %w", err)
+	}
+	mainRepository, err := repository.NewPostgresRepository(pool)
+	if err != nil {
+		log.Logger.Error("could not create db repository")
+		panic(err)
+	}
+	eventRepository := repository.NewInMemoryEventRepository()
+
+	g := run.Group{}
+
+	client := yunikorn.NewRESTClient(&cfg.YunikornConfig)
+	service := yunikorn.NewService(mainRepository, eventRepository, client)
+	g.Add(
+		func() error {
+			return service.RunEventCollector(ctx)
+		},
+		func(err error) {
+			service.Shutdown()
+		},
+	)
+	g.Add(
+		func() error {
+			return service.RunDataSync(ctx)
+		},
+		func(err error) {},
+	)
+
+	healthService := health.New(info.Version, health.NewYunikornComponent(client), health.NewPostgresComponent(pool))
+
+	ws := webservice.NewWebService(cfg.Port, mainRepository, eventRepository, healthService)
+	g.Add(
+		func() error {
+			return ws.Start(ctx)
+		},
+		func(err error) {
+			_ = ws.Shutdown(ctx)
+		},
+	)
+
+	if err = g.Run(); err != nil {
+		log.Logger.Warnf("group stopped because of an error: %v", err)
+	}
+
+	return nil
 }
 
 func New() *cobra.Command {
