dd50b802b1e9234679f1749c4c8021b1b08c34e1
Sudipto Baral <sudiptobaral.me@gmail.com>
2024-07-17 10:52:57 -0400

    Performance testing using k6 for event handler (#40)
    * Implement performance test using k6 #34
    
    * Improve script and makefile for perf test.
    
    ---------
    
    Co-authored-by: Dejan Zele Pejchev <pejcev.dejan@gmail.com>

--- hack/run-tests.sh
+++ hack/run-tests.sh
@@ -3,36 +3,74 @@
 set -e
 
 TEST_TYPE=$1
-if [ "$TEST_TYPE" != "integration" ] && [ "$TEST_TYPE" != "e2e" ]; then
-    echo "Invalid test type: $TEST_TYPE. Please provide either 'integration' or 'e2e' as the first argument."
+if [ "$TEST_TYPE" != "integration" ] && [ "$TEST_TYPE" != "e2e" ] && [ "$TEST_TYPE" != "performance" ]; then
+    echo "Invalid test type: $TEST_TYPE. Please provide either 'integration', 'e2e' or 'performance' as the first argument."
     exit 1
 fi
 
-# Cleanup the kind cluster when the script exits
+YHS_SERVER=${YHS_SERVER:-http://localhost:8989}
+
+# Cleanup the kind cluster and yunikorn-history-server process when the script exits
 cleanup () {
     echo "**********************************"
     echo "Deleting kind cluster"
     echo "**********************************"
-    KIND_CLUSTER=yhs-integration-tests make kind-delete-cluster
+    KIND_CLUSTER=yhs-test make kind-delete-cluster
+
+    if [ "$TEST_TYPE" == "performance" ]; then
+        echo "**********************************"
+        echo "Terminating yunikorn-history-server"
+        echo "**********************************"
+        pkill -f yunikorn-history-server
+    fi
 }
 trap cleanup EXIT
+
+wait_for_yhs() {
+    echo "**********************************"
+    echo "Waiting for yunikorn history server to start"
+    echo "**********************************"
+    while true; do
+        echo "Sending request to yunikorn history server..."
+        url="$YHS_SERVER/ws/v1/health/readiness"
+        status_code=$(curl --write-out %{http_code} --silent --output /dev/null $url || true)
+
+        if [ "$status_code" -eq 200 ] ; then
+            echo "Yunikorn history server is up and running."
+            break
+        else
+            echo "Waiting for yunikorn history server to start..."
+            sleep 10
+        fi
+    done
+}
 
 echo "**********************************"
 echo "Creating kind cluster"
 echo "**********************************"
-KIND_CLUSTER=yhs-integration-tests make kind-create-cluster
+KIND_CLUSTER=yhs-test make kind-create-cluster
 
 echo "**********************************"
 echo "Install and configure dependencies"
 echo "**********************************"
-make install-dependencies
+make install-dependencies migrate-up
 
-echo "**********************************"
-echo "Apply database migrations"
-echo "**********************************"
-make migrate-up
+if [ "$TEST_TYPE" == "performance" ]; then
+    echo "**********************************"
+    echo "Run yunikorn history server"
+    mkdir -p test-reports/performance
+    make clean build
+    bin/app/yunikorn-history-server --config config/yunikorn-history-server/local.yml > test-reports/performance/yhs.log &
+
+    # Wait for yunikorn history server to start
+    wait_for_yhs
+fi
 
 echo "**********************************"
 echo "Running $TEST_TYPE tests"
 echo "**********************************"
-make "test-go-$TEST_TYPE"
+if [ "$TEST_TYPE" == "performance" ]; then
+    make "test-k6-$TEST_TYPE"
+else
+    make "test-go-$TEST_TYPE"
+fi
---
+++ test/performance/event_handler_test.js
@@ -0,0 +1,58 @@
+import { Kubernetes } from 'k6/x/kubernetes';
+import http from 'k6/http';
+import { Trend } from 'k6/metrics';
+
+let podsSubmitted = new Trend('pods_submitted');
+let eventsProcessed = new Trend('events_processed');
+
+const namespace = __ENV.NAMESPACE || 'default';
+const yhsServer = __ENV.YHS_SERVER || 'http://localhost:8989';
+
+const podTemplate = {
+    apiVersion: 'v1',
+    kind: 'Pod',
+    metadata: {
+        generateName: 'test-pod-',
+        labels: {
+            app: 'performance-by-k6'
+        },
+        namespace: namespace
+    },
+    spec: {
+        containers: [
+            {
+                name: 'test-container',
+                image: 'nginx:latest',
+                ports: [{ containerPort: 80 }]
+            }
+        ]
+    }
+};
+
+export const options = {
+    stages: [
+        { duration: '5s', target: 5 },
+        { duration: '5s', target: 5 },
+        { duration: '5s', target: 0 },
+    ],
+};
+
+const getStat = () => {
+    const res = http.get(`${yhsServer}/ws/v1/event-statistics`);
+    return JSON.parse(res.body);
+};
+
+export default function () {
+    const kubernetes = new Kubernetes();
+    try {
+        kubernetes.create(podTemplate);
+    } catch (error) {
+        console.log(`Failed to create pod: ${error}`);
+    }
+    const json = getStat();
+    const eventProcessedByYHS = json["APP-ADD"]
+    const pods = kubernetes.list("Pod", namespace);
+    // Add the number of pods submitted and the number of events processed to the metrics
+    podsSubmitted.add(pods.length);
+    eventsProcessed.add(eventProcessedByYHS);
+}
