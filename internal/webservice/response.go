bf735f8d023c9de93a411310ced806c752278fd2
Dejan Zele Pejchev <pejcev.dejan@gmail.com>
2024-08-07 17:14:59 +0200

    add support for query params for application endpoints (#129)
    Signed-off-by: Dejan Zele Pejchev <pejcev.dejan@gmail.com>

--- internal/webservice/response.go
+++ internal/webservice/response.go
@@ -49,3 +49,19 @@ func errorResponse(w http.ResponseWriter, r *http.Request, err error) {
 		log.Logger.Errorf("could not write error response: %v", err)
 	}
 }
+
+func badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Logger.Errorf("error processing request for %s: %v", r.URL.Path, err)
+	problemDetails := ProblemDetails{
+		Type:     "about:blank",
+		Title:    "Bad Request",
+		Status:   http.StatusBadRequest,
+		Detail:   err.Error(),
+		Instance: r.URL.Path,
+	}
+	w.Header().Set("Content-Type", "application/problem+json")
+	w.WriteHeader(http.StatusBadRequest)
+	if err := json.NewEncoder(w).Encode(problemDetails); err != nil {
+		log.Logger.Errorf("could not write error response: %v", err)
+	}
+}
