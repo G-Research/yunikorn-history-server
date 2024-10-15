1222454fac435cc72cc07742c021c4b8f889ad7d
sudipto baral <sudiptobaral.me@gmail.com>
2024-10-14 20:36:24 -0400

    Add logic to append allocation and reservation.

--- internal/model/node.go
+++ internal/model/node.go
@@ -10,6 +10,42 @@ type Node struct {
 	dao.NodeDAOInfo `json:",inline"`
 }
 
-func (n *Node) MergeFrom(other *dao.NodeDAOInfo) {
-	n.NodeDAOInfo = *other
+func (n *Node) MergeFrom(nodeInfo *dao.NodeDAOInfo) {
+	n.NodeID = nodeInfo.NodeID
+	n.HostName = nodeInfo.HostName
+	n.RackName = nodeInfo.RackName
+	n.Attributes = nodeInfo.Attributes
+	n.Capacity = nodeInfo.Capacity
+	n.Allocated = nodeInfo.Allocated
+	n.Occupied = nodeInfo.Occupied
+	n.Available = nodeInfo.Available
+	n.Utilized = nodeInfo.Utilized
+	n.Allocations = nodeInfo.Allocations
+	n.Schedulable = nodeInfo.Schedulable
+	n.IsReserved = nodeInfo.IsReserved
+
+	lookup := make(map[string]struct{})
+	if len(nodeInfo.Allocations) > 0 {
+		for _, alloc := range n.Allocations {
+			lookup[alloc.AllocationKey] = struct{}{}
+		}
+		for _, alloc := range nodeInfo.Allocations {
+			if _, ok := lookup[alloc.AllocationKey]; !ok {
+				n.Allocations = append(nodeInfo.Allocations, alloc)
+			}
+		}
+		clear(lookup)
+	}
+
+	if len(nodeInfo.Reservations) > 0 {
+		for _, res := range n.Reservations {
+			lookup[res] = struct{}{}
+		}
+		for _, res := range nodeInfo.Reservations {
+			if _, ok := lookup[res]; !ok {
+				n.Reservations = append(nodeInfo.Reservations, res)
+			}
+		}
+		clear(lookup)
+	}
 }
